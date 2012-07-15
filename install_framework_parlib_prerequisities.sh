e03280bf8da3426ca6d26db36594afb6e891c217
paskma <paskma@gmail.com>
2012-07-16 00:45:53 +0300

    Do not use sudo

--- install_framework_parlib_prerequisities.sh
+++ install_framework_parlib_prerequisities.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install git-core
-sudo apt-get install subversion
-sudo apt-get install g++
-sudo apt-get install libgc-dev
-sudo apt-get install openjdk-6-jdk
-sudo apt-get install unzip
+apt-get install git-core
+apt-get install subversion
+apt-get install g++
+apt-get install libgc-dev
+apt-get install openjdk-6-jdk
+apt-get install unzip
 
