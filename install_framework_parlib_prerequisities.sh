ada639dee007613692d4d03d5f9eab6c9ab1daa3
paskma <paskma@gmail.com>
2012-07-15 23:55:41 +0300

    Sudo to all commands.

--- install_framework_parlib_prerequisities.sh
+++ install_framework_parlib_prerequisities.sh
@@ -3,7 +3,7 @@
 sudo apt-get install git-core
 sudo apt-get install subversion
 sudo apt-get install g++
-apt-get install libgc-dev
-apt-get install openjdk-6-jdk
-apt-get install unzip
+sudo apt-get install libgc-dev
+sudo apt-get install openjdk-6-jdk
+sudo apt-get install unzip
 
