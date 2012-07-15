bff75e1a840b5205f1f5846faa70b727a91b2557
Marek Paska <paskma@gmail.com>
2012-07-15 21:42:42 +0200

    Framework-parlib installations scripts for Debian.

---
+++ install_framework_parlib_prerequisities.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install git-core
+sudo apt-get install subversion
+sudo apt-get install g++
+apt-get install libgc-dev
+apt-get install openjdk-6-jdk
+apt-get install unzip
+
