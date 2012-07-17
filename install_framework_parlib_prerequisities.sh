877cd8a7a3f14d34cd02c4b5f0d3b30ba410d64f
Marek Paska <paskma@gmail.com>
2012-07-17 12:04:14 +0200

    Ability to insert -y switch.

--- install_framework_parlib_prerequisities.sh
+++ install_framework_parlib_prerequisities.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt-get install git-core
-apt-get install subversion
-apt-get install g++
-apt-get install libgc-dev
-apt-get install openjdk-6-jdk
-apt-get install unzip
+apt-get install git-core $@
+apt-get install subversion $@
+apt-get install g++ $@
+apt-get install libgc-dev $@
+apt-get install openjdk-6-jdk $@
+apt-get install unzip $@
 
